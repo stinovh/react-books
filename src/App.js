5fb2401edda915b0e830eff72df990e08ec0937a
stinovh <stijn.vanhaute@gmail.com>
2017-09-09 00:11:26 +0200

    added book component, loading books in state

--- src/App.js
+++ src/App.js
@@ -11,13 +11,20 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
+    books: [],
     showSearchPage: false
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
 
     BooksAPI.getAll().then((books) => {
-      console.log(books[0].shelf)
+      console.log(books)
     })
     return (
       <div className="app">
@@ -43,7 +50,9 @@ class BooksApp extends React.Component {
             </div>
           </div>
         ) : (
-          <BookList/>
+          <BookList
+            books={this.state.books}
+          />
         )}
       </div>
     )
--- src/BookList.js
+++ src/BookList.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Shelf from './Shelf'
 
-class BookList extends Component {
-  render() {
-    const shelfStatuses = ['currentlyReading', 'wantToRead', 'read']
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {shelfStatuses.map( (status) =>
-              <Shelf
-                key={status}
-                status={status}
-              />
-            )}
-          </div>
-        </div>
-        <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+const BookList = ({ books }) => {
+  const shelfStatuses = ['currentlyReading', 'wantToRead', 'read']
+
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          {shelfStatuses.map( (status) =>
+            <Shelf
+              key={status}
+              status={status}
+              books={books.filter((book) => book.shelf === status)}
+            />
+          )}
         </div>
       </div>
-    )
-  }
+      <div className="open-search">
+        <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+      </div>
+    </div>
+  )
 }
 
 export default BookList
