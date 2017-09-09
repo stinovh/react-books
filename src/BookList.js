074ac49eb4cdf84d7eebf959942a26ebdf9024ab
stinovh <stijn.vanhaute@gmail.com>
2017-09-09 21:25:31 +0200

    booklist to functional component

--- src/BookList.js
+++ src/BookList.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import Shelf from './Shelf'
 import PropTypes from 'prop-types'
 
-class BookList extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    changeShelf: PropTypes.func.isRequired
-  }
+const BookList = ({ books, changeShelf }) => {
+  const shelfStatuses = ['currentlyReading', 'wantToRead', 'read']
 
-  render() {
-    const shelfStatuses = ['currentlyReading', 'wantToRead', 'read']
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {shelfStatuses.map( (status) =>
-              <Shelf
-                key={status}
-                status={status}
-                books={this.props.books.filter((book) => book.shelf === status)}
-                changeShelf={this.props.changeShelf}
-              />
-            )}
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to='/search'>Add a Book</Link>
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          {shelfStatuses.map( (status) =>
+            <Shelf
+              key={status}
+              status={status}
+              books={books.filter((book) => book.shelf === status)}
+              changeShelf={changeShelf}
+            />
+          )}
         </div>
       </div>
-    )
-  }
+      <div className="open-search">
+        <Link to='/search'>Add a Book</Link>
+      </div>
+    </div>
+  )
+}
+
+BookList.propTypes = {
+  books: PropTypes.array.isRequired,
+  changeShelf: PropTypes.func.isRequired
 }
 
 export default BookList
